9aafda842f304198aa6d4df528baedabbec4fdce
Damian Michalowski <dowski888@gmail.com>
2022-06-05 08:38:38 +0200

    local storage in redux-saga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeLatest, call, put, takeEvery, select} from "redux-saga/effects"
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice"
+
+function* fetchExampleTasksHandler() {
+    try {
+       const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak!");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
