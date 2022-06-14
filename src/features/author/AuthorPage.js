3206f12bc802180aad1c5da6a842394423a5340b
Damian Michalowski <dowski888@gmail.com>
2022-06-14 11:34:39 +0200

    Author description changed, line high fix

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -11,13 +11,15 @@ export default () => (
             body={
                  <Content>
                     Nazwyam się <strong>Damian Michałowski</strong>, ukończyłem szkołę średnią
-                    o profilu matematyczno fizyczno informatycznym z rozszerzonym <strong>językiem angielskim na poziomie B1/B2</strong>, studiowałem
+                    o profilu matematyczno-fizyczno-informatycznym dwujęzycznym z <strong>językiem angielskim</strong> na poziomie <strong>B1/B2</strong>, studiowałem
                     na Politechnice Białostockiej przez 1 rok. Interesuję się <strong> programowaniem, grafiką komputerową, 
-                    montażem filmów i fotografią</strong>. <b>Pomimo młodego wieku</b> przez ostatnie 5 lat zdobywałem doświadczenie
-                    jako druga kamera w firmie zajmującęj się nagrywaniem i składaniem filmów z przeróżncyh uroczystości między innymi wesel, ślubów i 
-                    komunii. Ponadto <strong>pracowałem jako grafik w firmie zajmującą się tworzeniem stron, wizytówek, oraz logotypów.</strong> 
-                    Samodzielnie prowadziłem blog ukazujący moje własne fotografie i prace graficzne oraz nagrane teledyski. Aktualnie przeznaczam swój 
-                    czas w 100% na naukę programowania i mam nadzieje że w przyszłości będę wykonywał zawód programisty.
+                    montażem filmów i fotografią</strong>. Pomimo młodego wieku <strong>w ciągu ostatnich 5 lat zdobywałem doświadczenie</strong> między innymi
+                    jako druga kamera w firmie nagrywającej wesela i śluby. Ponadto <strong>pracowałem jako grafik</strong> w firmie zajmującą się tworzeniem stron, wizytówek, 
+                    oraz logotypów. Zajmowałem się <strong>freelancingiem</strong> odnośnie fotografii/obróbki zdjęć i tym podobnych. Prowadziłem <strong>blog</strong> ukazujący moje własne 
+                    fotografie, prace graficzne oraz filmy. Z <b>programowaniem</b> mam styczność od rozpoczęcia nauki w liceum gdzie miałem <strong>rozszerzoną informatykę</strong>,
+                    przygotowałem się do <strong>rozszerzonej matury z informatyki</strong> posługująć się <strong>C++</strong>, na studiach informatycznych nauczyłem się podstaw języka C, aktualnie 
+                    uczę się na własną rękę z pomocą <strong>kursu youcode</strong>, wszystkie technologie potrzebne do pracy jako Junior Front-End-Developer między innymi <strong>html, css, React,
+                    JavaScript(ES6+) i wiele innych</strong>. Czerpię przyjemność z programowania i wiążę z nim przyszłość.
                 </Content>
                 
             }
--- src/features/author/styled.js
+++ src/features/author/styled.js
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const Content = styled.p`
     text-align: justify;
+    line-height: 1.5;
 `;
